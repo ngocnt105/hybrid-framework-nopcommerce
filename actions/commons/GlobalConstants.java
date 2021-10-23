1abebeb4db360b0e72f5bb9bc682b24baf8a5898
ngocn105 <ngocnguyenuet@gmail.com>
2021-10-23 20:42:02 +0700

    Create automation framework

---
+++ actions/commons/GlobalConstants.java
@@ -0,0 +1,12 @@
+package commons;
+
+public class GlobalConstants {
+	//Url
+	//Username (app)
+	//Password (app)
+	//Server/ Enviroment
+	//Timeout
+	//Folder download/ upload
+	//...
+
+}
---
+++ testcases/com/nopcommerce/user/User_01_Register.java
@@ -0,0 +1,19 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class User_01_Register {
+  @Test
+  public void TC_01() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
